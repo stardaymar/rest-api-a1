d0dc1eb0a2dae7e572dbcf530e6dc5a09468a62d
Day <dayrosaa99@gmail.com>
2022-10-06 00:47:42 -0400

    Agregada rutas para buscar trabajos, buscar trabajo por id, por tipo, agregar trabajos. actualizar, eliminar y relacionar con los equipos

--- src/router/router.js
+++ src/router/router.js
@@ -6,18 +6,29 @@ const {
   delete: deleteEquipo,
   post,
 } = require("../controller/equipos.controller");
+const {
+  get: getTrabajos,
+ getTrabajosById,
+ getTrabajoByTipo,
+ deleteTrabajo,
+ putTrabajo,
+ postTrabajo
+} = require("../controller/trabajos.controller");
 const router = Router();
 
 router.get("/equipos", get);
-
 router.get("/equipos/:id", getById);
-
 router.post("/equipos", post);
-
 router.put("/equipos/:id", put);
-
 router.delete("/equipos/:id", deleteEquipo);
 
+
+router.get("/trabajos",   getTrabajos);
+router.get("/trabajos/:id", getTrabajosById);
+router.get("/trabajos/tipo/:tipo", getTrabajoByTipo);
+router.post("/trabajos", postTrabajo);
+router.put("/trabajos/:id", putTrabajo);
+router.delete("/trabajos/:id", deleteTrabajo);
 module.exports = {
   router,
 };
