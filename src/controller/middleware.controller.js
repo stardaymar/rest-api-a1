5a93533bcc593b30a9f75f845880e5aee6af19f6
Day <dayrosaa99@gmail.com>
2022-10-05 23:34:07 -0400

    Creado middleware paea rutas no definidas

---
+++ src/controller/middleware.controller.js
@@ -0,0 +1,9 @@
+class MiddlewareNotFound {
+  notFoundRouter(req, res, next) {
+    res.status(404).json({
+      mensaje:'La ruta que intentas acceder no se encuentra definida.'
+    });
+  }
+}
+
+module.exports = new MiddlewareNotFound();
